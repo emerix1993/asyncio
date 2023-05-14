eecc23f44223bf3c05a18d745d52de47e6af6f71
emerix1993 <emerix@ukr.net>
2023-05-14 15:19:49 +0300

    -new_commit

--- Asyncio/asyncio_homework.py
+++ Asyncio/asyncio_homework.py
@@ -1,10 +1,11 @@
+from pprint import pprint as print
 import asyncio
 import httpx
-from pprint import pprint as print
 
 
 class ExchangeRates:
     _instance = None
+    _initialized = False
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -12,20 +13,36 @@ class ExchangeRates:
 
         return cls._instance
 
-    @classmethod
-    async def create(cls):
-        self = cls()
-        await self.fetch_from_api()
-        return self
+    def __init__(self) -> None:
+        if ExchangeRates._initialized:
+            return
 
-    async def fetch_from_api(self) -> None:
+        self.data = asyncio.run(self.fetch_from_api())
+        self.data1 = asyncio.run(self.get_rate())
+
+        ExchangeRates._initialized = True
+
+    async def fetch_from_api(self):
         async with httpx.AsyncClient() as client:
-            response = await client.get("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=UAH&apikey=PASTE_YOUR_API_KEY")
-            self.data = response.json()
+            url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=UAH&apikey=PASTE_YOUR_API_KEY"
+            response = await client.get(url)
+            await asyncio.sleep(1)
+            return response.json()
+
+    async def get_rate(self):
+        async with httpx.AsyncClient() as client:
+            url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=PASTE_YOUR_API_KEY"
+            response = await client.get(url)
+            await asyncio.sleep(1)
+            return response.json()
+
 
 async def main():
-    er = await ExchangeRates.create()
+    er = ExchangeRates()
+    er.data, er.data1 = await asyncio.gather(er.fetch_from_api(), er.get_rate())
     print(er.data)
+    print(er.data1)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
